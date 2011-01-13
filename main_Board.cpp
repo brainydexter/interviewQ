eb17bf34fb58c530733e49ec185220968034547e
unknown <IVC@.us1.ent.verizon.com>
2011-01-13 10:41:40 -0600

    error checks for incorrect board dimensions to main_Board.cpp

--- main_Board.cpp
+++ main_Board.cpp
@@ -6,19 +6,32 @@
 
 int (*gPrintFn)( const char * format, ... ) = 0;
 
+const int MAX_CAPACITY = 9;
+
 int main()
 {
 	gPrintFn = printf;
 
-	char boardLetters [] = { 'y', 'o', 'x',
+	char boardLetters [MAX_CAPACITY] = { 'y', 'o', 'x',
 							 'r', 'b', 'a',
 							 'y', 'e', 'd' 
 							};
-	
-	Board* board = new Board("words.txt", 3, 3, boardLetters);
-	board->ProcessAllWords();
 
-	delete board;
+	int numRows = 3;
+	int numCols = 3;
+	
+	if( numRows * numCols == MAX_CAPACITY)
+	{
+		Board* board = new Board("words.txt", numRows, numCols, boardLetters);
+		board->ProcessAllWords();
+
+		delete board;
+	}
+	else
+	{
+		gPrintFn("Board dimensions don't match to the board letters provided");
+	}
+
 	_CrtMemDumpAllObjectsSince(0);
 	return 1;
 }
